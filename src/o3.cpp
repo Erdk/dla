39d033431c7629a6c293cdd0047fefc0516994c4
Lukas Redynk <mr.erdk@gmail.com>
2017-12-18 01:54:17 +0100

    o1, o3
    - modify o1 to produce csv file with coordinates
    - add o3 with 3d lattice
    - add o1.pde to visualise effects (sample points are in
    data/results.csv)

--- src/o3.cpp
+++ src/o3.cpp
@@ -12,6 +12,7 @@ namespace po = boost::program_options;
 // lattice dimmensions
 int width;
 int height;
+int depth;
 
 // target cluster size
 int cl_size;
@@ -24,51 +25,47 @@ bool progress;
 class Particle {
 	public:
 		// position on lattice
-		int x, y;
+		int x, y, z;
 
-		Particle(int x_, int y_) {
+		Particle(int x_, int y_, int z_) {
 			x = x_;
 			y = y_;
+      z = z_;
 		}
 
 		// Brownian random walk
 		void randomWalk() {
-			// 0 1 2
-			// 3 _ 4
-			// 5 6 7
-			switch(rand() % 8) {
+      // up(0):   y++
+      // down(1): y--
+      // left(2): x--
+      // right(3): x++
+      // near(4): z--
+      // far(5): z++
+			switch(rand() % 6) {
 				case 0:
-					x--;
 					y++;
 					break;
 				case 1:
-					y++;
+					y--;
 					break;
 				case 2:
-					x++;
-					y++;
+					x--;
 					break;
 				case 3:
-					x--;
+					x++;
 					break;
 				case 4:
-					x++;
+					z--;
 					break;
 				case 5:
-					y--;
-					x--;
-				case 6:
-					y--;
-					break;
-				case 7:
-					x++;
-					y--;
+					z++;
 					break;
 			}
 
-			if (x < 0 || x > width || y < 0 || y > height) {
+			if (x < 0 || x > width || y < 0 || y > height || z < 0 || z > depth) {
 				x = rand() % width;
 				y = rand() % height;
+        z = rand() % depth;
 			}
 		}
 };
@@ -78,7 +75,7 @@ class Cluster {
 		std::vector<Particle*> particles;
 		Cluster() {
 			particles = std::vector<Particle*>();
-			particles.push_back(new Particle(width/2, height/2));
+			particles.push_back(new Particle(width/2, height/2, depth/2));
 		}
 
 		virtual ~Cluster() {
@@ -92,7 +89,7 @@ class Cluster {
 			if (collide) {
 				particles.push_back(*p);
 
-				*p = new Particle(rand() % width, rand() % height);
+				*p = new Particle(rand() % width, rand() % height, rand() % depth);
 
         if (progress) {
           std::cerr << particles.size() << " ";
@@ -102,8 +99,9 @@ class Cluster {
 
 		bool checkCollision(Particle* p) {
 			for (Particle* i : particles) {
-				if ((p->x == i->x - 1 || p->x == i->x || p->x == i->x + 1 ) &&
-						(p->y == i->y - 1 || p->y == i->y || p->y == i->y + 1)) {
+				if ((p->x == i->x - 1 || p->x == i->x || p->x == i->x + 1) &&
+						(p->y == i->y - 1 || p->y == i->y || p->y == i->y + 1) &&
+						(p->z == i->z - 1 || p->z == i->z || p->z == i->z + 1)) {
 					return true;
 				}
 			}
@@ -115,11 +113,12 @@ class Cluster {
 int main(int argc, const char* argv[]) {
 
 	// <-- handle options -->
-	po::options_description desc("Usage:");
+	po::options_description desc("Allowed options");
 	desc.add_options()
 		("help,h", "print this help message")
     ("width,w", po::value<int>(&width)->default_value(900), "width of lattice") 
 		("height,e", po::value<int>(&height)->default_value(900), "height of lattice")
+		("depth,d", po::value<int>(&depth)->default_value(900), "depth of lattice")
 		("cluster-size,n", po::value<int>(&cl_size)->default_value(1000), "target size of cluster")
 		("progress,p", "show progress")
     ("verbose,v", "verbose output")
@@ -160,14 +159,14 @@ int main(int argc, const char* argv[]) {
 
   // print parameters of simulation
   if (verbose) {
-    std::cout << "w: " << width << " h: " << height << " cl: " << cl_size << " progress: " << progress << std::endl;
+    std::cout << "w: " << width << " h: " << height << " d: " << depth << " cl: " << cl_size << " progress: " << progress << std::endl;
   }
 
 	// cluster with one point in the center of lattice
 	Cluster c = Cluster();
 
 	// first "walking particle"
-	Particle* walker = new Particle(rand() % width, rand() % height);
+	Particle* walker = new Particle(rand() % width, rand() % height, rand() % depth);
 
 	// run until there's 'cl_size' particles in cluster, part of them will be
 	// duplicated, but observed loss was about ~10%, which is more acceptable
@@ -182,9 +181,9 @@ int main(int argc, const char* argv[]) {
 	}
 
   std::ofstream output;
-  output.open("result.csv");
+  output.open("result_3d.csv");
 	for (Particle* p : c.particles) {
-		output << p->x << "," << p->y << "\n";
+		output << p->x << "," << p->y << "," << p->z << "\n";
 	}
   output.close();
 
